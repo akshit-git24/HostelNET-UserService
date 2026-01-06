825d5b31f01617852548a73129ebc45f6010119b
Akshit Sahore <akshitsahore2827@gmail.com>
2026-01-06 14:09:33 +0530

    Here  is the fianl, maybe???---2

--- UserService/urls.py
+++ UserService/urls.py
@@ -5,25 +5,28 @@ from django.conf.urls.static import static
 
 from ninja_extra import NinjaExtraAPI
 from ninja_jwt.controller import NinjaJWTDefaultController
-from accounts.controllers import CustomNinjaJWTController
 
-# -----------------------------
+from accounts.controllers import CustomNinjaJWTController
+from accounts.api import UserController   # auth endpoints live here
+
+# ---------------------------------
 # SINGLE ROOT API
-# -----------------------------
+# ---------------------------------
 api = NinjaExtraAPI(
     title="HostelNet Auth API",
     version="1.0.0",
 )
 
-# JWT + custom auth controllers
+# Register auth-related controllers
 api.register_controllers(
     NinjaJWTDefaultController,
     CustomNinjaJWTController,
+    UserController,
 )
 
-# -----------------------------
+# ---------------------------------
 # Django URLs
-# -----------------------------
+# ---------------------------------
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", api.urls),
